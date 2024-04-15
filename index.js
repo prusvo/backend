c799eba257db8635130cabf10c60d4a9b409e283
prusvo <prusvoo@gmail.com>
2024-04-15 21:59:32 +0200

    0.6

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express()
 const __dirname = path.resolve()
 const buildPath = path.join(__dirname, "../frontend/build")
 app.use(cors({
-    origin: ['http://3.71.202.234'],
+    origin: ['http://recipeee.c1.biz'],
     credentials: true
 }))
 
