05d80df5ca13d9aaa883f7b836fdafcfccd5d7a8
prusvo <prusvoo@gmail.com>
2024-04-18 15:42:31 +0200

    0.07

--- authController.js
+++ authController.js
@@ -96,27 +96,20 @@ class authController {
             }
         }
     }
-    async verify(req, res, next) {
-        const authorizationHeader = req.headers.authorization;
-        if (!authorizationHeader) {
-            return res.status(403).json({ message: 'User does not have a token' });
-        }
-    
-        const token = authorizationHeader.split(' ')[1];
+    async verify (req, res, next) {
+        const token = req.cookies.token;
         if (!token) {
+            return res.json({ message: 'User does not have a token' });
+        }
+        
+        // Перевірка, чи токен є валідним JWT
+        if (!jwt.verify(token, secret.code)) {
             return res.status(403).json({ message: 'Invalid token' });
         }
-    
-        try {
-            const decodedToken = jwt.verify(token, secret.code);
-            req.user = decodedToken;
-            next();
-        } catch (error) {
-            console.error(error);
-            return res.status(403).json({ message: 'Invalid token' });
-        }
+        
+        
+
     }
-    
     async logoutUser (req, res) {
         try{
             res.clearCookie('token')
