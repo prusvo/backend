d928f6f5ceee8ea7248fd19ac448e175e61ac608
prusvo <prusvoo@gmail.com>
2024-04-18 13:47:44 +0200

    0.07

--- index.js
+++ index.js
@@ -1,57 +1,52 @@
 import express from "express";
 import mongoose from "mongoose";
-import cors from 'cors'
+import cors from 'cors';
 import bodyParser from "body-parser";
-import path from "path"
-
+import path from "path";
 import cookieParser from "cookie-parser";
+import authRouter from "./routes/authRouter.js";
+import RecPageRouter from "./routes/RecPageRouter.js";
 
+const PORT = process.env.PORT || 5001;
+const API = 'mongodb+srv://admin:admin@cluster0.t1ju6q0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const app = express();
+const __dirname = path.resolve();
+const buildPath = path.join(__dirname, "../frontend/build");
 
-import authRouter from "./routes/authRouter.js"
-import RecPageRouter from "./routes/RecPageRouter.js"
-const PORT = process.env.PORT || 5001
-const API = 'mongodb+srv://admin:admin@cluster0.t1ju6q0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-const app = express()
-const __dirname = path.resolve()
-const buildPath = path.join(__dirname, "../frontend/build")
 app.use(cors({
     origin: ['http://recipeee.c1.biz'],
     credentials: true
-}))
+}));
 
-app.use(cookieParser())
-
-
-
-
-
+app.use(cookieParser());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use("/auth", authRouter);
+app.use("/recipe", RecPageRouter);
+app.use(express.static(buildPath));
 
 mongoose.connect(API)
     .then(() => {
-        console.log('Connected to Mongo DB')
-        app.use(express.json())
-        app.use(bodyParser.json());
-        app.use("/auth", authRouter)
-        app.use("/recipe", RecPageRouter)
-        app.use(express.static(buildPath))
-        app.get("*", (req, res) =>{
-            res.sendFile(path.join(buildPath, "index.html"), (err) => {
-                if(err){
-                    console.log(err);
-                    res.status(500).send("Internal Server Error");
-                }
-            });
-        });
+        console.log('Connected to Mongo DB');
 
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
+        // Перевірка, чи з'єднання з базою даних встановлено
+        if (!app.get('port')) {
+            // Запуск сервера тільки один раз
+            app.listen(PORT, () => {
+                console.log(`Server is running on port ${PORT}`);
+            });
+        }
     })
     .catch((error) => {
         console.error('Failed to connect to MongoDB:', error);
-        process.exit(1); // Exit the process if MongoDB connection fails
+        process.exit(1); // Вийти з процесу, якщо з'єднання з базою даних не вдалося
     });
 
-
-
-
+// Обробник для коректного завершення роботи сервера
+process.on('SIGINT', () => {
+    console.log('Shutting down server...');
+    mongoose.connection.close(() => {
+        console.log('MongoDB connection closed');
+        process.exit(0);
+    });
+});
