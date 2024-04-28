60f0f8ba668b3cda01e76e590cb3ddacc4c1b7c1
prusvo <prusvoo@gmail.com>
2024-04-28 23:19:09 +0200

    update 0.08

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const __dirname = path.resolve()
 const buildPath = path.join(__dirname, "../frontend/build")
 
 app.use(cors({
-    origin: ['http://recipeee.c1.biz', 'http://localhost:3000'],
+    origin: ['http://recipees.net','http://recipeee.c1.biz', 'http://localhost:3000'],
     credentials: true
 }))
 app.use(cookieParser())
